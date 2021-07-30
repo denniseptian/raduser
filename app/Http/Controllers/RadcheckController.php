0f3e9995d5668905dbe2a392e4bb7cb039cf4cb3
denni septian <dennix.apk@gmail.com>
2021-07-30 09:57:15 +0700

    add expired/active selection

--- app/Http/Controllers/RadcheckController.php
+++ app/Http/Controllers/RadcheckController.php
@@ -10,7 +10,7 @@ class RadcheckController extends Controller
     public function index()
     {
         //
-        $raddatas = Radcheck::orderBy('id', 'DESC'); 
+        $raddatas = Radcheck::orderBy('id', 'DESC')->get(); 
         
         return view('radcheck.index', ['raddatas' => $raddatas]); 
     }
